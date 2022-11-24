4c108244c66cad4c3fc162f5a2f186745ab1c0ed
ivanzilbermann <rincon_del_arte_dag@hotmail.com>
2022-11-24 16:51:09 -0300

    entregacoderz1

---
+++ container.js
@@ -0,0 +1,67 @@
+const knex = require('knex');
+import logger from "../loggers/Log4jsLogger.js";
+
+class Container {
+    constructor(config, tableName) {
+        this.config = config;
+        this.tableName = tableName;
+        this.knex = knex(this.config);
+    }
+    save = obj => {
+        this.knex(this.tableName).insert(obj)
+            .then(() => console.log('Saved'))
+            .catch(err => { console.log(err); throw err })
+            .finally(() => this.knex.destroy())
+    }
+    getById = async id => {
+        try {
+            let obj = await this.knex.from(this.tableName).select().table(this.tableName).where('id', id).first();
+            if (obj) {
+                return obj;
+            } else {
+                return { message: 'ERROR' };
+            }
+        } catch (err) {
+            logger.error(error);
+            return { message: 'ERROR' };
+        }
+    }
+    getAll = async () => {
+        try {
+            let objs = await this.knex.from(this.tableName).select('*');
+            return objs;
+        } catch (err) {
+            logger.error(error);
+            return [];
+        }
+    }
+    deleteById = async id => {
+        try {
+            this.knex.from(this.tableName).where('id', '=', id).del()
+            return { message: 'DONE!' };
+        } catch (err) {
+            logger.error(error);
+            return { message: 'ERROR' };
+        }
+    }
+    deleteAll = async () => {
+        try {
+            this.knex.from(this.tableName).del()
+            return { message: 'DONE!' }
+        } catch (err) {
+            logger.error(error);
+            return { message: 'ERROR' };
+        }
+    }
+    update = async obj => {
+        try {
+            this.knex.from(this.tableName).update(obj).update()
+            return { message: 'DONE!' };
+        } catch (err) {
+            logger.error(error);
+            return { message: 'ERROR' };
+        }
+    }
+}
+
+module.exports = Container;
